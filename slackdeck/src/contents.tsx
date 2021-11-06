64c9020de04cca5878951deca644580ef0547ab1
yamamoto-yuta <automatuX78@gmail.com>
2021-11-07 01:42:36 +0900

    💄 Maintain column UI #5

--- slackdeck/src/contents.tsx
+++ slackdeck/src/contents.tsx
@@ -6,10 +6,13 @@ import { ColumnConfig } from './Contract';
 
 import './contents.scss';
 
+const columnNameDefaultValue = (_: TemplateStringsArray, columnIndex: number) => `Column ${columnIndex}`;
+const columnDeleteButtonId = (_: TemplateStringsArray, columnIndex: number) => `col-del-btn-${columnIndex}`;
 const columnIframeId = (_: TemplateStringsArray, columnIndex: number) => `col-iframe-${columnIndex}`;
 const columnElementId = (_: TemplateStringsArray, columnIndex: number) => `col-el-${columnIndex}`;
 
-const Deck = () => {
+
+const Main = () => {
   const [newColDefaultConfig, setNewColDefaultConfig] = React.useState<ColumnConfig>();
   const columnList: ColumnConfig[] = [];
 
@@ -31,15 +34,37 @@ const Deck = () => {
   }
 
   const addNewColumn = (columnIndex: number) => {
+    // Column Header
+    // -- Column Name
+    let colName = document.createElement('input');
+    colName.type = 'text';
+    colName.value = columnNameDefaultValue`${columnIndex}`;
+    colName.className = 'form-control';
+
+    // -- Column Delete Button
+    let colDelBtn = document.createElement('button');
+    colDelBtn.id = columnDeleteButtonId`${columnIndex}`;
+    colDelBtn.className = 'btn btn-danger';
+    colDelBtn.innerText = 'X';
+
+    // -- Column Header
+    let colHeader = document.createElement('div');
+    colHeader.className = 'col-header';
+    colHeader.appendChild(colName);
+    colHeader.appendChild(colDelBtn);
+
+    // Slack
     let iframe = document.createElement('iframe');
     iframe.id = columnIframeId`${columnIndex}`;
     iframe.src = newColDefaultConfig.url;
 
+    // Column
     let column = document.createElement('div');
-    column.className = "column";
+    column.className = "column bg-dark";
     column.id = columnElementId`${columnIndex}`;
     column.style.minWidth = newColDefaultConfig.width;
     column.style.width = newColDefaultConfig.width;
+    column.appendChild(colHeader);
     column.appendChild(iframe);
 
     wrapper.appendChild(column);
@@ -77,4 +102,4 @@ newBody.appendChild(body);
 newBody.appendChild(wrapper);
 document.documentElement.appendChild(newBody);
 
-ReactDOM.render(<Deck />, deck);
+ReactDOM.render(<Main />, deck);
