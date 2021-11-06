e60a376e71409ea4bdc1a76a2fd5844b08e640ab
yamamoto-yuta <automatuX78@gmail.com>
2021-11-06 22:09:03 +0900

    ✨ Enable to add column #5

--- slackdeck/src/contents.tsx
+++ slackdeck/src/contents.tsx
@@ -1,16 +1,62 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import { ColumnConfig } from './Contract';
 
 import './contents.scss';
 
+const columnIframeId = (_: TemplateStringsArray, columnIndex: number) => `col-iframe-${columnIndex}`;
+const columnElementId = (_: TemplateStringsArray, columnIndex: number) => `col-el-${columnIndex}`;
+
 const Sidebar = () => {
-    return (
-        <div className="container-fluid my-3 text-center text-white">
-            <button className="btn btn-primary rounded-circle"><FontAwesomeIcon icon={faPlus} className="sidebar-icon-large" /></button>
-        </div>
-    )
+  const [newColDefaultConfig, setNewColDefaultConfig] = React.useState<ColumnConfig>();
+  const columnList: ColumnConfig[] = [];
+
+
+  useEffect(() => {
+    chrome.storage.sync.get(
+      ['newColDefaultConfig'],
+      function (value) {
+        if (value.newColDefaultConfig) {
+          setNewColDefaultConfig(value.newColDefaultConfig);
+        }
+      }
+    );
+  }, []);
+
+  const fixSlackDom = () => {
+    let pClient = document.getElementsByClassName('p-client')[0] as HTMLElement;
+    pClient.style.width = '100%';
+  }
+
+  const addNewColumn = (columnIndex: number) => {
+    let iframe = document.createElement('iframe');
+    iframe.id = columnIframeId`${columnIndex}`;
+    iframe.src = newColDefaultConfig.url;
+
+    let column = document.createElement('div');
+    column.className = "column";
+    column.id = columnElementId`${columnIndex}`;
+    column.style.minWidth = newColDefaultConfig.width;
+    column.style.width = newColDefaultConfig.width;
+    column.appendChild(iframe);
+
+    wrapper.appendChild(column);
+
+    columnList.push(newColDefaultConfig);
+
+    fixSlackDom();
+  }
+
+  return (
+    <div className="container-fluid my-3 text-center text-white">
+      <button
+        className="btn btn-primary rounded-circle"
+        onClick={() => addNewColumn(columnList.length)}
+      ><FontAwesomeIcon icon={faPlus} className="sidebar-icon-large" /></button>
+    </div>
+  )
 }
 
 const sidebar = document.createElement('div');
@@ -20,10 +66,14 @@ sidebar.classList.add('bg-dark');
 const body = document.body;
 body.id = 'mainBody';
 
+const wrapper = document.createElement('div');
+wrapper.id = 'wrapper';
+
 const newBody = document.createElement('body');
 newBody.id = 'newBody';
 newBody.appendChild(sidebar);
 newBody.appendChild(body);
+newBody.appendChild(wrapper);
 document.documentElement.appendChild(newBody);
 
 ReactDOM.render(<Sidebar />, sidebar);
