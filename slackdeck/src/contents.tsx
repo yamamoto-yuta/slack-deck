3217f9fefc2cc835c03587808ea7b672e8ec76b8
yamamoto-yuta <automatuX78@gmail.com>
2021-11-08 17:40:26 +0900

    ✨ Enable to save current column state #7

--- slackdeck/src/contents.tsx
+++ slackdeck/src/contents.tsx
@@ -14,80 +14,96 @@ const columnElementId = (_: TemplateStringsArray, columnIndex: number) => `col-e
 
 const Main = () => {
   const [newColDefaultConfig, setNewColDefaultConfig] = React.useState<ColumnConfig>();
-  const columnList: ColumnConfig[] = [];
+  let columnList: ColumnConfig[] = [];
 
 
   useEffect(() => {
     chrome.storage.sync.get(
-      ['newColDefaultConfig'],
+      ['newColDefaultConfig', 'columnList'],
       function (value) {
         if (value.newColDefaultConfig) {
           setNewColDefaultConfig(value.newColDefaultConfig);
         }
+        if (value.columnList) {
+          columnList = value.columnList;
+          console.log(columnList);
+          for (var i = 0; i < columnList.length; i++) {
+            wrapper.appendChild(addColumn(i, columnList[i]));
+          }
+        }
       }
     );
   }, []);
+
+  const addColumn = (columnIndex: number, columnCofig: ColumnConfig = newColDefaultConfig) => {
+    // Column
+    let column = document.createElement('div');
+    column.id = columnElementId`${columnIndex}`;
+    column.className = "column bg-dark";
+    column.style.minWidth = columnCofig.width;
+    column.style.width = columnCofig.width;
+
+    // Slack
+    let iframe = document.createElement('iframe');
+    iframe.id = columnIframeId`${columnIndex}`;
+    iframe.className = "col-iframe";
+    iframe.src = columnCofig.url;
+
+    // Column Header
+    let colHeader = document.createElement('div');
+    colHeader.className = 'col-header';
+
+    // -- Column Name
+    let colName = document.createElement('input');
+    colName.type = 'text';
+    colName.value = columnNameDefaultValue`${columnIndex}`;
+    colName.className = 'form-control';
+
+    // -- Column Delete Button
+    let colDelBtn = document.createElement('button');
+    colDelBtn.id = columnDeleteButtonId`${columnIndex}`;
+    colDelBtn.className = 'btn btn-danger col-del-btn';
+    colDelBtn.innerText = 'X';
+    colDelBtn.onclick = () => {
+      // Remove
+      var delcolIdx = parseInt(column.id.split('-').slice(-1)[0]);
+      columnList.splice(delcolIdx, 1);
+      column.remove();
+
+      // Update other elements
+      for (var i = 0; i < columnList.length; i++) {
+        document.getElementsByClassName('column')[i].id = columnElementId`${i}`;
+        document.getElementsByClassName('col-iframe')[i].id = columnIframeId`${i}`;
+        document.getElementsByClassName('col-del-btn')[i].id = columnDeleteButtonId`${i}`;
+      }
+    };
+
+    // Append elements
+    colHeader.appendChild(colName);
+    colHeader.appendChild(colDelBtn);
+    column.appendChild(colHeader);
+    column.appendChild(iframe);
+
+    return column;
+  }
 
   const fixSlackDom = () => {
     let pClient = document.getElementsByClassName('p-client')[0] as HTMLElement;
     pClient.style.width = '100%';
   }
 
-  const addNewColumn = (columnIndex: number) => {
+  const onClickAddNewColumnButton = (columnIndex: number) => {
     if (newColDefaultConfig !== undefined) {
+      // Add column
+      wrapper.appendChild(addColumn(columnIndex));
+      fixSlackDom();
 
-      // Column
-      let column = document.createElement('div');
-      column.id = columnElementId`${columnIndex}`;
-      column.className = "column bg-dark";
-      column.style.minWidth = newColDefaultConfig.width;
-      column.style.width = newColDefaultConfig.width;
-
-      // Slack
-      let iframe = document.createElement('iframe');
-      iframe.id = columnIframeId`${columnIndex}`;
-      iframe.className = "col-iframe";
-      iframe.src = newColDefaultConfig.url;
-
-      // Column Header
-      let colHeader = document.createElement('div');
-      colHeader.className = 'col-header';
-
-      // -- Column Name
-      let colName = document.createElement('input');
-      colName.type = 'text';
-      colName.value = columnNameDefaultValue`${columnIndex}`;
-      colName.className = 'form-control';
-
-      // -- Column Delete Button
-      let colDelBtn = document.createElement('button');
-      colDelBtn.id = columnDeleteButtonId`${columnIndex}`;
-      colDelBtn.className = 'btn btn-danger col-del-btn';
-      colDelBtn.innerText = 'X';
-      colDelBtn.onclick = () => {
-        // Remove
-        var delcolIdx = parseInt(column.id.split('-').slice(-1)[0]);
-        columnList.splice(delcolIdx, 1);
-        column.remove();
-
-        // Update other elements
-        for (var i = 0; i < columnList.length; i++) {
-          document.getElementsByClassName('column')[i].id = columnElementId`${i}`;
-          document.getElementsByClassName('col-iframe')[i].id = columnIframeId`${i}`;
-          document.getElementsByClassName('col-del-btn')[i].id = columnDeleteButtonId`${i}`;
-        }
-      };
-
-      // Append elements
-      colHeader.appendChild(colName);
-      colHeader.appendChild(colDelBtn);
-      column.appendChild(colHeader);
-      column.appendChild(iframe);
-      wrapper.appendChild(column);
-
+      // Push to columnList
       columnList.push(newColDefaultConfig);
 
-      fixSlackDom();
+      // Save current column state
+      chrome.storage.sync.set({ 'columnList': columnList });
+
     } else {
       alert('Please set default column config first.\nYou will need to reload to reflect the settings.');
     }
@@ -97,7 +113,7 @@ const Main = () => {
     <div className="mx-2 my-3 text-center text-white">
       <button
         className="btn btn-primary rounded-circle"
-        onClick={() => addNewColumn(columnList.length)}
+        onClick={() => onClickAddNewColumnButton(columnList.length)}
       ><FontAwesomeIcon icon={faPlus} className="deck-icon-large" /></button>
     </div>
   )
