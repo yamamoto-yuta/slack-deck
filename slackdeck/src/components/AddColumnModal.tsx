8fafcf53c7fab89e916f6b9ed82f1191a6d422c7
yamamoto-yuta <automatuX78@gmail.com>
2021-11-20 02:20:57 +0900

    ♻️ コンポーネント分割 #34

---
+++ slackdeck/src/components/AddColumnModal.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+import { ColumnConfig, DEFAULT_WIDTH_OPTION, WIDTH_OPTION_LIST } from "../Contract";
+import { addColumn, saveColumns } from "../functions/column";
+
+const columnNameDefaultValue = (_: TemplateStringsArray, columnIndex: number) => `Column ${columnIndex}`;
+
+export const AddColumnModal: React.FC<{
+  show: boolean,
+  onHide: () => void,
+  columnList: Array<ColumnConfig>,
+  setSavedTime: React.Dispatch<React.SetStateAction<Date>>,
+}> = (props) => {
+  const [newColumnConfig, setNewColumnConfig] = React.useState<ColumnConfig>({
+    width: DEFAULT_WIDTH_OPTION.value,
+    url: "",
+    name: ""
+  });
+
+  React.useEffect(() => {
+    setNewColumnConfig({ ...newColumnConfig, name: columnNameDefaultValue`${props.columnList.length}` });
+  }, [props.show]);
+
+  const onClickAddButton = () => {
+    // Add column
+    document.getElementById('wrapper').appendChild(addColumn(props.columnList, props.setSavedTime, props.columnList.length, newColumnConfig));
+    // Fix slack dom
+    let pClient = document.getElementsByClassName('p-client')[0] as HTMLElement;
+    pClient.style.width = '100%';
+    // Push to columnList
+    props.columnList.push(newColumnConfig);
+    // Save current column state
+    saveColumns(props.columnList, props.setSavedTime);
+    // Close Mordal
+    props.onHide();
+  }
+
+  return (
+    <div>
+      <Modal show={props.show} onHide={props.onHide} centered className="text-dark">
+        <Modal.Header closeButton>
+          <Modal.Title>Add new column</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Enter the column width and URL of the column you want to add.</p>
+          <Form className="d-flex">
+            <Form.Select
+              className="w-auto mx-1"
+              onChange={(e) => setNewColumnConfig({ ...newColumnConfig, width: e.target.value })}
+            >
+              {WIDTH_OPTION_LIST.map((option) => (
+                <option
+                  value={option.value}
+                  selected={DEFAULT_WIDTH_OPTION.value === option.value}
+                >{option.text}</option>
+              ))}
+            </Form.Select>
+            <Form.Control
+              type="text"
+              className="mx-1"
+              placeholder="https://app.slack.com/client/*"
+              onChange={(e) => setNewColumnConfig({ ...newColumnConfig, url: e.target.value })}
+            />
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={() => onClickAddButton()}>
+            Add
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
---
+++ slackdeck/src/components/ConfigModal.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+import { GeneralConfig } from "../Contract";
+
+export const ConfigModal: React.FC<{
+  show: boolean,
+  onHide: () => void,
+  currentGeneralConfig: GeneralConfig,
+  setGeneralConfig: React.Dispatch<React.SetStateAction<GeneralConfig>>
+}> = (props) => {
+  const [updatedGeneralConfig, setUpdatedGeneralConfig] = React.useState<GeneralConfig>(props.currentGeneralConfig);
+
+  React.useEffect(() => {
+    setUpdatedGeneralConfig(props.currentGeneralConfig);
+  }, [props.show]);
+
+  const onClickSaveButton = () => {
+    props.setGeneralConfig(updatedGeneralConfig);
+    // Save config
+    chrome.storage.sync.set({ 'generalConfig': updatedGeneralConfig });
+    // Hide modal
+    props.onHide();
+    // Adapt config
+    document.getElementById('mainBody').classList.toggle('text-light');
+    document.getElementById('newBody').classList.toggle('text-light');
+  }
+
+  return (
+    <div>
+      <Modal show={props.show} onHide={props.onHide} centered className="text-dark">
+        <Modal.Header closeButton>
+          <Modal.Title>Config</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group>
+              <h5>Dark Theme</h5>
+              <div className="mb-3">
+                <Form.Check
+                  type={"checkbox"}
+                  checked={updatedGeneralConfig.useDarkTheme}
+                  label={"Use dark theme"}
+                  onChange={(e) => setUpdatedGeneralConfig({ useDarkTheme: e.target.checked })}
+                /></div>
+              <Form.Text className="text-muted">
+                Check this box if you want to use the dark theme in Slack.
+              </Form.Text>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={() => onClickSaveButton()}>
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
