cefaedd7d7e4ff4fa3327399d7027d28916e31c5
yamamoto-yuta <automatuX78@gmail.com>
2021-12-10 17:27:42 +0900

    🚧 入力欄の追加に対応 #82

--- slackdeck/src/components/ConfigModal.tsx
+++ slackdeck/src/components/ConfigModal.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Button, Col, Form, Modal, Row } from "react-bootstrap";
-import { CLIENT_URL_PATTERN, GeneralConfig, SlackUrlConverter, SlackUrlValidateResult, ValidationResult, WORKSPACE_URL_PATTERN } from "../Contract";
+import { CLIENT_URL_PATTERN, GeneralConfig, SlackUrlConverter, SlackUrlValidateResult, ValidationResult, VALIDATION_FAILED, VALIDATION_SUCCESS, WORKSPACE_URL_PATTERN } from "../Contract";
 
 const workspaceUrlInputName = (_: TemplateStringsArray, index: number) => `input-workspace-url-${index}`;
 const clientUrlInputName = (_: TemplateStringsArray, index: number) => `input-client-url-${index}`;
+const extractUrlInputIndexFromName = (urlInput: string) => parseInt(urlInput.split('-').slice(-1)[0]);
 
 export const ConfigModal: React.FC<{
   show: boolean,
@@ -27,13 +28,15 @@ export const ConfigModal: React.FC<{
     }
     setValidateUrl(validationResult);
 
+    setIsValidAllUrl(true);
+
   }, [props.show]);
 
   const urlValidator = (regex: RegExp, url: string) => {
     if (regex.test(url)) {
-      return { isValid: true, message: "" };
+      return VALIDATION_SUCCESS;
     } else {
-      return { isValid: false, message: "Does not match format." };
+      return VALIDATION_FAILED;
     }
   };
 
@@ -78,6 +81,22 @@ export const ConfigModal: React.FC<{
     setIsValidAllUrl(isValidAllSlackUrl());
   };
 
+  const onClickAddSlackUrlInputFormButton = () => {
+    // Update satate
+    let newSlackUrlTable: SlackUrlConverter[] = updatedGeneralConfig.slackUrlTable.slice();
+    newSlackUrlTable.push({ workspaceUrl: "", clientUrl: "" });
+    setUpdatedGeneralConfig({ ...updatedGeneralConfig, slackUrlTable: newSlackUrlTable });
+    // Validate
+    let newSlackUrlValidateResult: SlackUrlValidateResult[] = validateUrl.slice();
+    newSlackUrlValidateResult.push({
+      workspaceUrl: VALIDATION_FAILED,
+      clientUrl: VALIDATION_FAILED,
+    });
+    setValidateUrl(newSlackUrlValidateResult);
+    // Update All validation result
+    setIsValidAllUrl(false);
+  }
+
   const onClickSaveButton = () => {
     props.setGeneralConfig(updatedGeneralConfig);
     // Save config
@@ -120,7 +139,6 @@ export const ConfigModal: React.FC<{
           <p>Correspond <code>https://[workspace_url].slack.com/</code> and <code>https://app.slack.com/client/*/</code>.</p>
           <div>
             {updatedGeneralConfig.slackUrlTable.map((converter, index) => {
-              console.log(converter);
               return (
                 <div className="d-flex my-1">
                   <Form className="flex-fill">
@@ -129,7 +147,7 @@ export const ConfigModal: React.FC<{
                       name={workspaceUrlInputName`${index}`}
                       value={converter.workspaceUrl}
                       placeholder="https://[workspace_url].slack.com/"
-                      onChange={(e) => onChangeWorkspaceUrl(e.target.value, parseInt(e.target.name))}
+                      onChange={(e) => onChangeWorkspaceUrl(e.target.value, extractUrlInputIndexFromName(e.target.name))}
                     />
                     <Form.Text className="text-danger">{validateUrl[index].workspaceUrl.message}</Form.Text>
                   </Form>
@@ -140,7 +158,7 @@ export const ConfigModal: React.FC<{
                       className="flex-fill"
                       value={converter.clientUrl}
                       placeholder="https://app.slack.com/client/XXXXXXXXXXX/"
-                      onChange={(e) => onChangeClientUrl(e.target.value, parseInt(e.target.name))}
+                      onChange={(e) => onChangeClientUrl(e.target.value, extractUrlInputIndexFromName(e.target.name))}
                     />
                     <Form.Text className="text-danger">{validateUrl[index].clientUrl.message}</Form.Text>
                   </Form>
@@ -156,7 +174,7 @@ export const ConfigModal: React.FC<{
           </div>
           <Button
             variant="primary"
-          // onClick={onClickSaveButton}
+            onClick={onClickAddSlackUrlInputFormButton}
           >
             +
           </Button>
@@ -164,7 +182,7 @@ export const ConfigModal: React.FC<{
         <Modal.Footer>
           <Button
             variant="primary"
-            disabled={!(isValidAllSlackUrl)}
+            disabled={!(isValidAllUrl)}
             onClick={onClickSaveButton}
           >
             Save
