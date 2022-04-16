35c63840e9d9b5ff032328a938191c64741779c4
yamamoto-yuta <automatuX78@gmail.com>
2022-04-16 15:29:42 +0900

    👍 AddColumnModalを仮実装

--- slackdeck/src/components/AddColumnModal.tsx
+++ slackdeck/src/components/AddColumnModal.tsx
@@ -1,50 +1,11 @@
 import React from 'react';
-import { SelectChangeEvent, IconButton, Modal, Box, Typography, Divider, FormControl, Select, MenuItem } from '@mui/material';
-import SettingsIcon from '@mui/icons-material/Settings';
-import { COLUMN_WIDTH_OPTIONS_TEXT } from '../consts/columnWidth';
+import ReactDOM from 'react-dom';
+import { SelectChangeEvent, Modal, Box, Typography, Divider, FormControl, Select, MenuItem, TextField, Button, InputLabel } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import { COLUMN_WIDTH_OPTIONS_TEXT } from '../shared/columnWidth';
+import "../style/addColumnModal.scss";
+import { Column } from './Column';
 
-const DefaultColumnWidthSelect: React.FC = () => {
-  const [selectedColumnWidth, setSelectedColumnWidth] = React.useState<string>("");
-
-  const handleChange = (event: SelectChangeEvent) => {
-    setSelectedColumnWidth(event.target.value as string);
-  };
-  return (
-    <div>
-      <Typography variant="h5" gutterBottom>
-        Default column width
-      </Typography>
-      <Typography variant="body1" gutterBottom>
-        When adding columns, the column width set here is used as the default value.
-      </Typography>
-      <FormControl fullWidth>
-        <Select
-          value={selectedColumnWidth}
-          onChange={handleChange}
-        >
-          {COLUMN_WIDTH_OPTIONS_TEXT.map((option, index) => (
-            <MenuItem key={index} value={option}>
-              {option}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </div>
-  )
-};
-
-const WorkspaceName2IdMapper: React.FC = () => {
-  return (
-    <div>
-      <Typography variant="h5" gutterBottom>
-        Map workspace name to workspace ID
-      </Typography>
-      <Typography variant="body1" gutterBottom>
-        By mapping a workspace named URL <code>https://[WORLSPACE_NAME].slack.com</code> to a workspace ID URL <code>https://app.slack.com/[WORKSPACEID]</code>, columns can be added from a workspace named URL.
-      </Typography>
-    </div>
-  )
-};
 
 export const AddColumnModal: React.FC<{
   open: boolean,
@@ -61,23 +22,63 @@ export const AddColumnModal: React.FC<{
     boxShadow: 24,
     p: 4,
   };
+  const [selectedColumnWidth, setSelectedColumnWidth] = React.useState<string>("");
+  const handleSelectedColumnWidthChange = (event: SelectChangeEvent) => {
+    setSelectedColumnWidth(event.target.value as string);
+  };
+  const onClickAddButton = () => {
+    let col = document.createElement('div');
+    ReactDOM.render(<Column />, col);
+    document.getElementById('wrapper').appendChild(col);
+
+    props.onClose();
+  };
   return (
     <div>
-      {console.log(props.open)}
       <Modal
         open={props.open}
         onClose={props.onClose}
       >
         <Box sx={style}>
           <Typography variant="h4">
-            Config
+            Add new column
           </Typography>
           <Divider sx={{ my: 1 }} />
-          <DefaultColumnWidthSelect />
+
+          <TextField label="URL" variant="outlined" sx={{ my: 1 }} fullWidth />
+
+          <FormControl fullWidth>
+            <InputLabel id="column-width-select-label">Column Width</InputLabel>
+            <Select
+              labelId="column-width-select-label"
+              label="Column Width"
+              value={selectedColumnWidth}
+              onChange={handleSelectedColumnWidthChange}
+            >
+              {COLUMN_WIDTH_OPTIONS_TEXT.map((option, index) => (
+                <MenuItem key={index} value={option}>
+                  {option}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          <TextField label="Column Name" variant="outlined" sx={{ my: 1 }} fullWidth />
+
           <Divider sx={{ my: 1 }} />
-          <WorkspaceName2IdMapper />
+
+          <div id="add-column-button">
+            <Button
+              variant="contained"
+              startIcon={<AddIcon />}
+              onClick={onClickAddButton}
+            >
+              Add
+            </Button>
+          </div>
+
         </Box>
       </Modal>
-    </div>
+    </div >
   )
 };
