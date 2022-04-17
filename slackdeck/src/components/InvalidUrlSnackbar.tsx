235a43bb8e8533097d7e9b4698357dca5698d37e
yamamoto-yuta <automatuX78@gmail.com>
2022-04-17 19:59:49 +0900

    💄 デザインを向上

--- slackdeck/src/components/InvalidUrlSnackbar.tsx
+++ slackdeck/src/components/InvalidUrlSnackbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IconButton, Snackbar } from '@mui/material';
+import { Alert, IconButton, Snackbar } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 
 export const InvalidUrlSnackbar: React.FC<{
@@ -33,8 +33,11 @@ export const InvalidUrlSnackbar: React.FC<{
       open={props.open}
       autoHideDuration={3000}
       onClose={handleClose}
-      message={`Clipboard URL is invalid: "${props.clipboardText.length > 10 ? props.clipboardText.slice(0, 10) + '...' : props.clipboardText}"`}
       action={action}
-    />
+    >
+      <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+        {`Clipboard URL is invalid: "${props.clipboardText.length > 10 ? props.clipboardText.slice(0, 10) + '...' : props.clipboardText}"`}
+      </Alert>
+    </Snackbar>
   )
 };
