9327ad99d691c49b4c4f89f6a71f4d3bdb56f96e
yamamoto-yuta <automatuX78@gmail.com>
2022-04-17 20:59:09 +0900

    💄 UIを作成

--- slackdeck/src/popup/Popup.tsx
+++ slackdeck/src/popup/Popup.tsx
@@ -1,5 +1,11 @@
+import { ExpandLess, ExpandMore, StarBorder } from "@mui/icons-material";
+import { List, ListSubheader, ListItemButton, ListItemIcon, ListItemText, Collapse } from "@mui/material";
+import HistoryIcon from '@mui/icons-material/History';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 import React, { useEffect } from "react";
 import "./Popup.scss";
+import { VERSION } from "../shared/general";
 
 export default function Popup() {
   useEffect(() => {
@@ -7,5 +13,31 @@ export default function Popup() {
     chrome.runtime.sendMessage({ popupMounted: true });
   }, []);
 
-  return <div className="popupContainer">Hello, world!</div>;
+  return (
+    <div className="popupContainer">
+      <List
+        sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+        subheader={
+          <ListSubheader component="div" id="nested-list-subheader">
+            SlackDeck v{VERSION}
+          </ListSubheader>
+        }
+      >
+        <ListItemButton>
+          <ListItemIcon><HistoryIcon /></ListItemIcon>
+          <ListItemText primary="Release Note" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon><GitHubIcon /></ListItemIcon>
+          <ListItemText primary="Repository" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon><ShoppingBagIcon /></ListItemIcon>
+          <ListItemText primary="Chrome Web Store" />
+        </ListItemButton>
+      </List>
+    </div>
+  )
 }
